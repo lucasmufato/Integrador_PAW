41860f87f2185d1014106b2ece1b49ccdaf0478b
lucasmufato <l.mufato@gmail.com>
2017-05-30 21:58:12 -0300

    login_vista

--- php/Views/Login_View.php
+++ php/Views/Login_View.php
@@ -1,25 +1,50 @@
 <?php
     //esta clase se encarga de checkear la sesion, y redirigir en caso que este logeado.
-    include_once("../Session_Controler.php");
+    include_once("../Controlers/Session_Controler.php");
     //si sigue por aca es q no esta logueado
 ?>
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
+    <meta name="google-signin-client_id" content="YOUR_CLIENT_ID.apps.googleusercontent.com">
     <title>login</title>
-    <link rel="stylesheet" href=".css">
-    <script></script>
+    <link rel="stylesheet" href="../../css/login/login.css">
+    <script src="../../js/login.js"></script>
+    <!-- script para logearse con G+ -->
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
 </head>
 <body>
-    <header>
-	    <h1> Logeo </h1>
-    </header>
     <section>
-        <form action="" id="login">
+        <h1>Inicia sesion!</h1>
+        <form id="login">
+            <label for="">Nombre de Usuario</label>
+            <input type="text" min="3" max="50" required="true">
+            <label for="">Password</label>
+            <input type="password" min="8" max="50" required="true">
+            <label for="">Remember me</label>
+            <input type="checkbox" checked="checked" id="rememberMe"> 
+            <p class="boton verde"> Ingresar </p>
         </form>
+        
         <!--   BOTON DE GOOGLE    -->
+        <div class="g-signin2" data-onsuccess="onSignIn"> </div>
+        
+        <h2>no tienes cuenta? hacete una!</h2>
         <form action="" id="newResearcher">
+            <label for="">Nombre</label>
+            <input type="text" min="3" max="50" required="true">
+            <label for="">Apellido</label>
+            <input type="text" min="3" max="50" required="true">
+            <label for="">Email</label>
+            <input type="email" min="5" max="70" required="true">
+            <label for="">Nombre de Usuario</label>
+            <input type="text" min="3" max="50" required="true">
+            <label for="">Password</label>
+            <input type="password" min="8" max="50" required="true">
+            <label for="">Ingresa tu password otra vez</label>
+            <input type="password" min="8" max="50" required="true">
+            <p class="boton verde"> Registrarse </p>
         </form>
     </section>
 </body>
